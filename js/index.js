424fea96f5bf1fc3064a41d652ce86565815cf09
mengxain0913 <mengxian0913@gmail.com>
2023-01-20 17:41:31 +0800

    loading

--- js/index.js
+++ js/index.js
@@ -85,10 +85,17 @@ setInterval(function() {
 }, 3000);
 
 
-var section = document.getElementById("sec4");
-var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-if (isSafari) {
-    section.style.backgroundAttachment = "scroll";
-} else {
-    section.style.backgroundAttachment = "fixed";
-}
+// var section = document.getElementById("sec4");
+// var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+// if (isSafari) {
+//     section.style.backgroundAttachment = "scroll";
+// } else {
+//     section.style.backgroundAttachment = "fixed";
+// }
+
+/*Loading*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".load").style.display = "none";
+    document.querySelector(".containerF").style.display = "block";
+});
